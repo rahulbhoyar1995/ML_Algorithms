b3e07dad0fab597e43ffb3231a1c9f9a87ede9f3
Rahul Bhoyar <rahulbhoyaroffice@gmail.com>
2023-04-06 18:10:42 +0530

    adding file

--- 1_Regression_Problem/scripts/a_simple_regression.py
+++ 1_Regression_Problem/scripts/a_simple_regression.py
@@ -1,7 +1,22 @@
 import pandas as pd 
 import numpy as np
 
-
-
-df = pd.read_csv("datasets/movie_dataset.csv")
-print(df)
+class RegressionModel:
+    """
+    This is simple Regression Model used for predicting the Box Office
+    Collection of Hollywood movies based on certain predictors
+    """
+    test_data = ""
+    train_data = ""
+    
+    def creating_dataframe(self, csv_file_location : str) -> pd.DataFrame:
+        df = pd.read_csv(csv_file_location)
+        return df
+    
+    @classmethod
+    def creating_training_data(cls, df):
+        
+    
+    
+test_df = RegressionModel().creating_dataframe("/Users/rahulbhoyar/Desktop/Rahul_PC/projects/ML_Algorithms/1_Regression_Problem/datasets/movie_dataset.csv")
+print(test_df)
